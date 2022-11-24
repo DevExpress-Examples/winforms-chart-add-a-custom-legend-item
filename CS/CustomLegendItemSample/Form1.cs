e488c522e2c416cf6a7089935a8dece87650d7f3
ritazakhodyaeva <57389482+ritazakhodyaeva@users.noreply.github.com>
2022-11-24 17:10:08 +0400

    Update Form1.cs

--- CS/CustomLegendItemSample/Form1.cs
+++ CS/CustomLegendItemSample/Form1.cs
@@ -4,8 +4,7 @@ using System;
 using System.IO;
 using System.Windows.Forms;
 
-namespace CustomLegendItemSample
-{
+namespace CustomLegendItemSample {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
